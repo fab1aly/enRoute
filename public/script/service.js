87ef2849c5033ee3d77170c016236d7f6ed315d8
fab1aly <fabien.aly@gmail.com>
2024-01-02 20:45:27 +0100

    worker

--- public/script/service.js
+++ public/script/service.js
@@ -1,8 +1,31 @@
-const button = document.getElementById("notifications");
-button.addEventListener("click", () => {
-    Notification.requestPermission().then((result) => {
-        // if (result === "granted") {
-        //     randomNotification();
-        // }
-    });
+// const button = document.getElementById("notifications");
+// button.addEventListener("click", () => {
+//     Notification.requestPermission().then((result) => {
+//         // if (result === "granted") {
+//         //     randomNotification();
+//         // }
+//     });
+// });
+
+self.addEventListener("fetch", event => {
+    // This is a dummy event listener
+    // just to pass the PWA installation criteria on 
+    // some browsers
+    console.log(`fetch ok`);
+});
+
+
+// This code executes in its own worker or thread
+self.addEventListener("install", event => {
+    console.log("Service worker installed");
+});
+self.addEventListener("activate", event => {
+    console.log("Service worker activated");
+});
+
+
+Notification.requestPermission().then((result) => {
+    if (result === "granted") {
+        console.log("Notif ok");
+    }
 });
