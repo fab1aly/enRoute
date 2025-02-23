61cd8f1b5970ea56518aead97737409759a075be
fab1aly <fabien.aly@gmail.com>
2025-02-23 19:08:11 +0100

    update modal

--- public/script/home.js
+++ public/script/home.js
@@ -15,42 +15,16 @@ if (window.localStorage.getItem('local_list')) {
 local_list.displayInit();
 local_list.setViewOnAllPoint();
 
-////////////////////////////////////////////////////////////////////////////////
-// Save list in db (open modal)
-const save_button = document.querySelector(`#listpoint #save_button`);
-if (save_button) {
-    save_button.addEventListener('click', () => {
-        const modal = document.querySelector("#modal");
-        modal.style.display = "flex";
-        event.preventDefault();
-
-    });
-}
 
 ////////////////////////////////////////////////////////////////////////////////
-// Modal listner
-const modal = document.querySelector("#modal");
-
 // When clicks on the button, remove list (submit)
-const button = document.querySelector("#modal button");
-button.addEventListener('click', () => {
-
+const save_button = document.querySelector("#modal_save_route button");
+save_button.addEventListener('click', () => {
     const listpoint = JSON.stringify(local_list.getList());
-    document.querySelector('#listpoint #save_listpoint').value = listpoint;
-
-    const input = document.querySelector("#modal input");
-    document.querySelector('#listpoint #save_listname').value = input.value;
-
-    document.querySelector('#listpoint #save_form').submit();
+    document.querySelector('#save_listpoint').value = listpoint;
+    document.querySelector('#modal_save_route form').submit();
 });
 
-// When clicks on cross (x) or modal , close the modal
-const cross = document.querySelector("#modal .cross i");
-document.addEventListener('click', () => {
-    if (event.target == modal || event.target == cross) {
-        modal.style.display = "none";
-    }
-});
 
 ////////////////////////////////////////////////////////////////////////////////
 // init Position
@@ -71,8 +45,6 @@ if (!local_list.getList().length) {
         map.setView([e.coords.latitude, e.coords.longitude], 13);
     });
 }
-
-
 
 
 // });
--- public/script/routes.js
+++ public/script/routes.js
@@ -1,8 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     removeRoute = function (uniqid) {
-//         console.log(uniqid);
-//     }
-// });
+
 
 // function for toggle select li (route) in ul (routes)
 function displaySelect(li) {
@@ -45,9 +41,6 @@ if (list_routes.querySelector('li')) {
         console.log('route_list is empty');
     }
 }
-function removeRoute(uniqid) {
-    console.log(uniqid);
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 // routes ul listener
@@ -75,48 +68,8 @@ document.querySelector('#routes ul').addEventListener('click', () => {
         input.value = li.dataset.uniqid;
     }
 
-    // remove route
-    if (event.target.matches('.remove i')) {
-        const li = event.target.closest('li');
-
-        const input = document.querySelector('#remove_form input');
-        input.value = li.dataset.uniqid;
-
-        // display modal
-        const modal = document.querySelector("#modal");
-        modal.style.display = "flex";
-
-        // display modal
-        const legend = modal.querySelector("fieldset legend");
-        legend.textContent = "Attention";
-        const span = modal.querySelector("fieldset span");
-        span.textContent = "Voulez-vous supprimer cette route ?";
-        const p = modal.querySelector("fieldset p");
-        p.textContent = li.dataset.name;
-        const button = modal.querySelector("button");
-        button.style.display = "inline";
-    }
 
 
-    // share route
-    if (event.target.matches('.share i')) {
-        const li = event.target.closest('li');
-
-        // display modal
-        const modal = document.querySelector("#modal");
-        modal.style.display = "flex";
-
-        // edit modal
-        const legend = modal.querySelector("fieldset legend");
-        legend.textContent = "Partage";
-        const span = modal.querySelector("fieldset span");
-        span.textContent = " Voici un lien pour partager cette route ";
-        const p = modal.querySelector("fieldset p");
-        p.textContent = "enRoute.ovh/route?id=" + li.dataset.uniqid;
-        const button = modal.querySelector("button");
-        button.style.display = "none";
-
-    }
 });
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -132,34 +85,10 @@ if (listElement) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// Modal listner
-const modal = document.querySelector(".modal");
-
-// When clicks on the button, remove list (submit)
-const button = document.querySelector("#modal button");
-if (button) {
-    button.addEventListener('click', () => {
-        document.querySelector('#remove_form').submit();
-    });
-}
-
-// When clicks on cross (x) or modal , close the modal
-const cross = document.querySelector("#modal .cross i");
-document.addEventListener('click', () => {
-    if (event.target == modal || event.target == cross) {
-        modal.style.display = "none";
-    }
-});
-
-
-////////////////////////////////////////////////////////////////////////////////
 
 // init Position
 import Position from "./module/Position.js";
 const pos = new Position(map);
-
-
-
 
 
 // });
